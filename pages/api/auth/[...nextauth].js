c1552efd21aded346ec3333e3e6ec75870593731
sebastianluna-dev <sebastian.luna.developer@gmail.com>
2023-01-21 00:21:41 -0600

    fix: use env variables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GithubProvider from 'next-auth/providers/github';
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: '3ec38f845ca16683c18a',
-      clientSecret: '0017cc47a10ee71e22c58264c1f3928c9d9a0260',
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 });
