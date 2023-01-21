d82bceb77d45822449b5b5af391bb9a5a3c1ac37
sebastianluna-dev <sebastian.luna.developer@gmail.com>
2023-01-21 01:34:12 -0600

    fix: add secret in api/auth/[...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
+  secret: process.env.SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
